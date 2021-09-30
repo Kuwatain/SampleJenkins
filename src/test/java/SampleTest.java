54ad4459cd54aee941e8c2cbc2ba656f2c9d972d
Kuwatain <tonyrave94@gmail.com>
2021-09-30 23:07:21 +0300

    init

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.Test;
+
+public class SampleTest {
+
+    @Test
+    void test1() {
+        System.out.println("Init");
+        System.out.println("Steps");
+        System.out.println("Assertions");
+    }
+
+    @Test
+    void test2() {
+        System.out.println("Init");
+        System.out.println("Steps");
+        System.out.println("Assertions");
+    }
+
+    @Test
+    void test3() {
+        System.out.println("Init");
+        System.out.println("Steps");
+        System.out.println("Assertions");
+    }
+
+    @Test
+    void test4() {
+        System.out.println("Init");
+        System.out.println("Steps");
+        System.out.println("Assertions");
+    }
+}
